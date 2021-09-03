041aa79cb9904faa4ffcbcdc0b256e95762d9732
Radif <mail@radif.ru>
2021-09-03 19:48:54 +0300

    Создание представлений для отображения товаров и Добавления товара

--- django_shop/goods_app/views.py
+++ django_shop/goods_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView, CreateView, ListView
 
-# Create your views here.
+from .forms import GoodsForm
+from .models import Goods
+
+
+class GoodsModelListView(ListView):
+    template_name = 'goods_app/index.html'
+    queryset = Goods.objects.all()
+
+
+class GoodsModelFormView(FormView, CreateView):
+    template_name = 'goods_app/create_good.html'
+    form_class = GoodsForm
+    success_url = reverse_lazy('goods')
