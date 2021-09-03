06e84c709cc7a5738ed4f574eaea09e19b58cd89
Radif <mail@radif.ru>
2021-09-03 22:37:50 +0300

    select_related для оптимизации запросов из множества в 1 с помощью join

--- django_shop/goods_app/views.py
+++ django_shop/goods_app/views.py
@@ -6,11 +6,14 @@ from .models import Goods
 
 
 class GoodsModelListView(ListView):
-    template_name = 'goods_app/index.html'
-    queryset = Goods.objects.all()
+    template_name = 'index.html'
+    # Группировка запросов для OneToOne
+    queryset = Goods.objects.select_related('supplier').all()
+    # Группировка запросов для ManyToMany
+    # queryset = Goods.objects.prefetch_related('supplier').all()
 
 
 class GoodsModelFormView(FormView, CreateView):
-    template_name = 'goods_app/create_good.html'
+    template_name = 'create_good_form.html'
     form_class = GoodsForm
     success_url = reverse_lazy('goods')
