aa79c3eb344e584cea8d46d127b305b448e9e744
Radif <mail@radif.ru>
2021-09-03 22:37:12 +0300

    Прячем админку в новый url

--- django_shop/django_shop/urls.py
+++ django_shop/django_shop/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('default_admin/', admin.site.urls),
 
     path('', include('goods_app.urls'))
 ]
